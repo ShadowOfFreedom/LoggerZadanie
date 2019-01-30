11ebbb6c1774617a9468c36d1fd765669de09af5
Wojciech Wojtkielewicz <MJBR64@motorolasolutions.com>
2019-01-30 14:23:55 +0100

    Zrobione całe zadanie wraz z obsługą błedów oraz logowaniem błedów do pliku

---
+++ LoggerTester/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LoggerTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.Logger logger = new Logger.Logger();
+
+            Console.Write("Podaj nazwę pliku:");
+            var fileName = Console.ReadLine();
+
+            while (true)
+            {
+                Console.WriteLine("Podaj tekst wiadomości:");
+                var text = Console.ReadLine();
+                try
+                {
+                    logger.Log(text);
+                }
+                catch (Exception e)
+                {
+                    Console.Write($"Wystąpił błąd {e.Message}, w jakim plku chcesz go zapisać:");
+                    var errorFileName = Console.ReadLine();
+
+                    logger.Log(e.Message, errorFileName);
+                }
+
+            }
+
+        }
+    }
+}
